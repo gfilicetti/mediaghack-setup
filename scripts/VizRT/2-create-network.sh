c66f2c40eee22acf38a3a2ff60f970e897cacb5b
Gino Filicetti <ginof@google.com>
2025-06-18 13:16:13 -0400

    added firewall rule creation

--- scripts/VizRT/2-create-network.sh
+++ scripts/VizRT/2-create-network.sh
@@ -3,5 +3,6 @@ REGION=${2:-"us-central1"}
 RANGE=${3:-"10.1.0.0/16"}
 PROJECT=${4:-`gcloud config get-value project`}
 
-gcloud compute networks create default --subnet-mode=custom
-gcloud compute networks subnets create us-central1 --range=$RANGE --stack-type=IPV4_ONLY --network=$NETWORK --region=$REGION
+gcloud compute networks create default --subnet-mode=custom --project=$PROJECT 
+gcloud compute networks subnets create us-central1 --range=$RANGE --stack-type=IPV4_ONLY --network=$NETWORK --region=$REGION --project=$PROJECT 
+gcloud compute firewall-rules create allow-remotes --direction=INGRESS --priority=1000 --network=$NETWORK --action=ALLOW --rules=tcp:22,tcp:80,tcp:443,tcp:3389 --source-ranges=0.0.0.0/0 --project=$PROJECT 
