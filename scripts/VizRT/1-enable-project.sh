8c0b02260456c0c2bcea0c979d502d82616238ed
Gino Filicetti <ginof@google.com>
2025-06-18 12:34:23 -0400

    fixed typo

--- scripts/VizRT/1-enable-project.sh
+++ scripts/VizRT/1-enable-project.sh
@@ -1,7 +1,7 @@
 PROJECT=${1:-`gcloud config get-value project`}
 
 # enable the Compute API
-gcloud services enable compute.googleapis.com --project-$PROJECT
+gcloud services enable compute.googleapis.com --project=$PROJECT
 
 # GF: This is for Argo environments only
 gcloud org-policies reset constraints/compute.vmExternalIpAccess --project=$PROJECT
