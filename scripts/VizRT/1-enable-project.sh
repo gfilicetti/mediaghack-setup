469b1b309148595b11a72b121e0d6c2d73618cbc
Gino Filicetti <ginof@google.com>
2025-06-18 12:35:40 -0400

    added org policy api enablement

--- scripts/VizRT/1-enable-project.sh
+++ scripts/VizRT/1-enable-project.sh
@@ -4,4 +4,5 @@ PROJECT=${1:-`gcloud config get-value project`}
 gcloud services enable compute.googleapis.com --project=$PROJECT
 
 # GF: This is for Argo environments only
+gcloud services enable orgpolicy.googleapis.com --project=$PROJECT
 gcloud org-policies reset constraints/compute.vmExternalIpAccess --project=$PROJECT
