563674c51d416ce069961b63ad107032481fcb55
Gino Filicetti <ginof@google.com>
2025-06-18 12:43:27 -0400

    updated vm and license vm options

--- scripts/VizRT/3-create-vizrt-vm.sh
+++ scripts/VizRT/3-create-vizrt-vm.sh
@@ -1,5 +1,5 @@
 VM_NAME=${1:-"`gcloud config get-value project`-tricaster"}
-REGION=${2:-"us-central1"}
+ZONE=${2:-"us-central1-a"}
 RANGE=${3:-"10.1.0.0/16"}
 PROJECT=${4:-`gcloud config get-value project`}
 
@@ -7,14 +7,14 @@ gcloud compute addresses create $VM_NAME-ip --region=$REGION
 IP=$(gcloud compute addresses describe $VM_NAME-ip --region=$REGION --format="get(address)")
 
 gcloud compute instances create $VM_NAME \
-	--region=$REGION --machine-type=g2-standard-16 \
+	--zone=$ZONE --machine-type=g2-standard-16 \
 	--network-interface=address=$IP,network-tier=PREMIUM,nic-type=GVNIC,stack-type=IPV4_ONLY,subnet=$REGION \
 	--accelerator=count=1,type=nvidia-l4 \
 	--tags=http-server,https-server,lb-health-check \
-	--create-disk=auto-delete=yes,boot=yes,device-name=viz-tricaster-1,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20250613,mode=rw,size=50,type=pd-balanced \
-	--create-disk=auto-delete=yes,device-name=disk-1,mode=rw,name=viz-tricaster-4-disk-1,size=70,type=pd-balanced \
-	--create-disk=auto-delete=yes,device-name=disk-2,mode=rw,name=viz-tricaster-4-disk-2,size=200,type=pd-balanced \
-	--create-disk=auto-delete=yes,device-name=disk-3,mode=rw,name=viz-tricaster-4-disk-3,size=500,type=pd-ssd \
+	--create-disk=auto-delete=yes,boot=yes,device-name=$VM_NAME,image=projects/windows-cloud/global/images/windows-server-2022-dc-v20250613,mode=rw,size=50,type=pd-balanced \
+	--create-disk=auto-delete=yes,device-name=disk-1,mode=rw,name=$VM_NAME-disk-1,size=70,type=pd-balanced \
+	--create-disk=auto-delete=yes,device-name=disk-2,mode=rw,name=$VM_NAME-disk-2,size=200,type=pd-balanced \
+	--create-disk=auto-delete=yes,device-name=disk-3,mode=rw,name=$VM_NAME-disk-3,size=500,type=pd-ssd \
 	--shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring \
 	--metadata=enable-osconfig=TRUE,enable-oslogin=true \
 	--maintenance-policy=TERMINATE --provisioning-model=STANDARD
